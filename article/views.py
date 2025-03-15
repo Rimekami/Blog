6014da1d3431cb0c98223de85ba1d49ee07d4c62
Rimekami <emirhangokkoca9@gmail.com>
2025-03-16 01:32:21 +0300

    makaleler veritabanına kaydedildi - birkaç küçük yazı hatası düzeltildi

--- article/views.py
+++ article/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 from .forms import ArticleForm
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -13,5 +15,10 @@ def dashboard(request):
     return render(request, "dashboard.html")
 
 def addarticle(request):
-    form = ArticleForm()
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Makale başarıyla oluşturuldu.")
+        return redirect("index")
+
     return render(request, "addarticle.html",{"form":form})
