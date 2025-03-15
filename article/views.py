c7e225e050b0d15fa85ea4df77f911442fce7e8c
Rimekami <emirhangokkoca9@gmail.com>
2025-03-16 01:20:35 +0300

    makale sayfası oluşturuldu

---
+++ article/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .forms import ArticleForm
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+def addarticle(request):
+    form = ArticleForm()
+    return render(request, "addarticle.html",{"form":form})
