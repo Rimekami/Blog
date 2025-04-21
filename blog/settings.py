d8c20b252d5448b4534d3e49c222407c3cc4f583
Rimekami <143909031+Rimekami@users.noreply.github.com>
2025-04-21 12:28:42 +0300

    addarticle.html ve setting.py ckeditor ile düzenlendi

--- blog/settings.py
+++ blog/settings.py
@@ -141,3 +141,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CKEDITOR_CONFIGS = {
+    "default": {
+        "removePlugins": "stylesheetparser",
+    }
+}
