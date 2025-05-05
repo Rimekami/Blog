7a67aafa04cd8fcb03860254943a3baf98984894
Rimekami <143909031+Rimekami@users.noreply.github.com>
2025-05-05 13:27:29 +0300

    html ler ama çalışmıyor :D

--- article/views.py
+++ article/views.py
@@ -89,32 +89,3 @@ def addComment(request,id):
 
 
 
-
-from .models import Book
-from .forms import BookForm
-
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'book_list.html', {'books': books})
-
-def book_create(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'book_form.html', {'form': form})
-
-def book_update(request, pk):
-    book = Book.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'book_form.html', {'form': form})
-
