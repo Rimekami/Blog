946133620b86f446c72527dd7a343c82d560322f
Emirhan Gokkoca <emirhangokkoca9@gmail.com>
2025-06-24 06:40:08 +0300

    veri tabanı migrations hatası nedeniyle sıfırlandı

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status', 'rating', 'added_date']
+    list_filter = ['status', 'rating', 'added_date']
+    search_fields = ['title', 'author']
+    date_hierarchy = 'added_date'
+    
+    fieldsets = (
+        ('Temel Bilgiler', {
+            'fields': ('title', 'author', 'status', 'description')
+        }),
+        ('Detaylar', {
+            'fields': ('page_count', 'isbn', 'cover_image')
+        }),
+        ('Tarihler', {
+            'fields': ('start_date', 'finish_date')
+        }),
+        ('Değerlendirme', {
+            'fields': ('rating', 'review')
+        }),
+    )
